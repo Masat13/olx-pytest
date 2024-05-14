002aa3ba4aa427f69ab844a3f2beb0169540730b
Maks <m.hurylov@goit.ua>
2024-05-14 15:21:24 +0300

    readme

--- runner.py
+++ runner.py
@@ -1,4 +1,4 @@
 import subprocess
 
-s = subprocess.run(f'python -m pytest -s -v --alluredir=allure_report tests', shell=True, check=False)
+s = subprocess.run(f'python -m pytest -s -v --alluredir=allure_report', shell=True, check=False)
 # s = subprocess.run(f'allure serve allure_report --port 8001', shell=True, check=False)
