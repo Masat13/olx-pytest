80752bef0d5e57be23a6573f65601b271ae0e15f
Maks <m.hurylov@goit.ua>
2024-05-14 16:22:55 +0300

    allure docker

--- runner.py
+++ runner.py
@@ -6,4 +6,4 @@ s = subprocess.run(f'python -m pytest -s -v --alluredir=allure_report', shell=Tr
 # s = subprocess.run(f'allure serve allure_report --port 8001', shell=True, check=False)
 # docker jenkins report generate and copy to jenkins
 subprocess.run('allure generate allure_report -o allure_report_html', shell=True, check=False)
-shutil.copytree('allure_report', 'C:\Users\imasa\.jenkins\workspace\docker job', dirs_exist_ok=True)
+shutil.copytree('allure_report', 'C:\\Users\\imasa\\.jenkins\\workspace\\docker job', dirs_exist_ok=True)
