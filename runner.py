fb25b3c36fe3ef950c45543f817d757ffbd43406
Maks <m.hurylov@goit.ua>
2024-05-16 12:50:41 +0300

    comment

--- runner.py
+++ runner.py
@@ -1,6 +1,6 @@
 import subprocess
 
 s = subprocess.run(f'python -m pytest -s -v --alluredir=allure_report', shell=True, check=False)
-# local report
-s = subprocess.run(f'allure serve allure_report --port 8001', shell=True, check=False)
+# local report , don`t use it with Jenkins
+# s = subprocess.run(f'allure serve allure_report --port 8001', shell=True, check=False)
 
